747a3cec28c1edff028912ada4954d68ce948b03
huwei <huwei@huansi.net>
2024-05-22 18:21:40 +0800

    支持对对象类型的数据进行类型标记，比如byte[]

---
+++ ConsoleApp1/JsonReaderExtend.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public static class JsonReaderExtend
+    {
+        public static void ReadAndAssert(this JsonReader jsonReader)
+        {
+            if (!jsonReader.Read())
+            {
+                throw new JsonSerializationException( "Unexpected end when reading JSON.");
+            }
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
@@ -6,6 +7,7 @@ using System.Data;
 using System.IO;
 using System.Linq;
 using System.Net.Security;
+using System.Runtime;
 using System.Runtime.Serialization.Json;
 using System.Text;
 
@@ -18,14 +20,22 @@ namespace ConsoleApp1
             // 创建一个DataTable
             DataTable dataTable = new DataTable("MyTable");
             dataTable.Columns.Add("ID", typeof(int));
+
+            dataTable.Columns.Add("gData", typeof(byte[]));
             dataTable.Columns.Add("Name", typeof(string));
 
             DataRow row1 = dataTable.NewRow();
             row1["ID"] = 1;
             row1["Name"] = "John Doe";
+            row1["gData"] = new byte[] { 10, 20, 30 };
             dataTable.Rows.Add(row1);
 
 
+            DataRow row2 = dataTable.NewRow();
+            row1["ID"] = 2;
+            row1["Name"] = "huwei";
+            row1["gData"] = new byte[] { 30, 20, 30 };
+            dataTable.Rows.Add(row2);
 
 
 
@@ -41,6 +51,22 @@ namespace ConsoleApp1
             table = DataTableSerializer.Deserialize(json);
             Console.WriteLine($"有{table.Rows.Count}行数据,{table.Columns.Count}列");
 
+            row1 = table.NewRow();
+            row1["ID"] = 1;
+            row1["Name"] = "John Doe";
+            row1["gData"] = new byte[] { 10, 20, 30 };
+            table.Rows.Add(row1);
+
+
+             row2 = table.NewRow();
+            row1["ID"] = 2;
+            row1["Name"] = "huwei";
+            row1["gData"] = new byte[] { 30, 20, 30 };
+            table.Rows.Add(row2);
+            json = DataTableSerializer.Serialize(table);
+            Console.WriteLine(json);
+            var newtable = DataTableSerializer.Deserialize(json);
+            Console.WriteLine($"有{newtable.Rows.Count}行数据,{newtable.Columns.Count}列");
             Console.ReadKey();
         }
 
@@ -49,6 +75,12 @@ namespace ConsoleApp1
 
     public class DataTableSerializer
     {
+        static DataTableSerializer()
+        {
+            settings = new JsonSerializerSettings() { TypeNameHandling = TypeNameHandling.Objects };
+            settings.Converters.Insert(0, new ConsoleApp1.DataTableConverter());
+        }
+        public static JsonSerializerSettings settings;
         private class InnerTable
         {
             public string Schema { get; set; }
@@ -61,13 +93,13 @@ namespace ConsoleApp1
                 var schemaTable = new DataTable();
                 using (var stream = new MemoryStream(Encoding.UTF8.GetBytes(Schema)))
                 {
-                    using (var reader =new System.Xml.XmlTextReader(stream))
+                    using (var reader = new System.Xml.XmlTextReader(stream))
                     {
                         schemaTable.ReadXmlSchema(reader);
                     }
                 }
                 //合并数据
-                if(Data != null && Data.Rows.Count > 0)
+                if (Data != null && Data.Rows.Count > 0)
                 {
                     schemaTable.Merge(Data);
                 }
@@ -80,7 +112,7 @@ namespace ConsoleApp1
         /// <param name="dataTable">要序列化的dataTable</param>
         /// <param name="includeSchemaAlways">是否总是带上schema,默认情况下有数据时，是不会带上schema的，但是当一行数据都没有时，会带上</param>
         /// <returns></returns>
-        public static string Serialize(DataTable dataTable,bool includeSchemaAlways=false)
+        public static string Serialize(DataTable dataTable, bool includeSchemaAlways = false)
         {
             InnerTable innerTable = new InnerTable();
             if (includeSchemaAlways || dataTable.Rows.Count == 0)
@@ -96,7 +128,7 @@ namespace ConsoleApp1
                 }
             }
             innerTable.Data = dataTable;
-            return JsonConvert.SerializeObject(innerTable);
+            return JsonConvert.SerializeObject(innerTable, settings);
         }
         /// <summary>
         /// 反序列化到DataTable
@@ -105,7 +137,7 @@ namespace ConsoleApp1
         /// <returns>DataTable</returns>
         public static DataTable Deserialize(string json)
         {
-            InnerTable innerTable = JsonConvert.DeserializeObject<InnerTable>(json);
+            InnerTable innerTable = JsonConvert.DeserializeObject<InnerTable>(json, settings);
             return innerTable.GetData();
         }
     }
